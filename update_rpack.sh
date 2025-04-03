5e54e3ce04988cbd9b50c60dc69a8ec4fbd48be7
Peter Pan <peter@never.lan>
2025-04-03 12:13:02 -0400

    update rpack

--- update_rpack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# stage old packfile removal
-mv resourcepacks/ithavollr-rpack.pw.toml resourcepacks/ithavollr-rpack1.old.toml
-
-# get the new file from our argument
-./packwiz url add ithavollr_rpack $1 --meta-folder resourcepacks
-
-# reconfigure the new packfile to add client & standardize name:
-sed -i '/^filename = "Ithavollr_rpack-.*\.zip"$/c\filename = "Ithavollr_rpack.zip"\nside = "client"' resourcepacks/ithavollr-rpack.pw.toml
